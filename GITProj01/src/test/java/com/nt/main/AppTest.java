f88fd99379adc26213848df10209084f38f6c21a
saiganesh <saiganesh@localhost>
2024-11-10 19:18:58 +0530

    improved task1 --- TL

--- GITProj01/src/test/java/com/nt/main/AppTest.java
+++ GITProj01/src/test/java/com/nt/main/AppTest.java
@@ -30,4 +30,13 @@ public class AppTest {
     	int actual=app.sum(-100, 200);
     	assertEquals(expected,actual);
     }
+    
+    @Test
+    public void testSumWithZerosValues() {
+    	App app= new App();
+    	int expected=0;
+    	int actual=app.sum(0,0);
+    	assertEquals(expected,actual);
+    }
+    
 }
