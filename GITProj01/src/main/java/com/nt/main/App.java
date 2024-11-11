9138184d1027c472c5df15ebacb432804beab379
saiganesh <saiganesh@localhost>
2024-11-11 18:26:23 +0530

    Dev1 change 22

--- GITProj01/src/main/java/com/nt/main/App.java
+++ GITProj01/src/main/java/com/nt/main/App.java
@@ -10,6 +10,6 @@ public class App {
         
     	App app= new App();
     	
-    	System.out.println("sum result is:"+app.sum(10, 20));
+    	System.out.println("sum result22 is:"+app.sum(10, 20));
     }
 }
